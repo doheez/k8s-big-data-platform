2be6a9cb877842b52580163ddd52f775712521f0
idoburnish <doyun7433@naver.com>
2022-04-07 03:08:38 +0900

    #4: Spring 개발 환경 설정

---
+++ server/src/test/java/com/example/K8s/K8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.K8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
