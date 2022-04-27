e72edfafbb3c6554dd0b178e111ba1ed3dbcfa1a
doheez <doheedev@gmail.com>
2022-04-27 21:49:35 +0900

    feat: Create App Bar component
    화면 최상단에 반응형 메뉴바를 생성합니다.
    메뉴는 Main, My Page, Log In/Out으로 구성됩니다.
    
    Task: #6

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import ResponsiveAppBar from './components/AppBar/ResponsiveAppBar';
+
 function App() {
   return (
-    <div>컴포넌트 개발 예정</div>
+    <ResponsiveAppBar />
   );
 }
 
