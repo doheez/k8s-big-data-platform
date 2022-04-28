db1505a8bc97bf861aa1d751cc06931d48f3c791
doheez <doheedev@gmail.com>
2022-04-28 12:15:21 +0900

    feat: Add folder image to banner
    배너 좌측에 폴더 이미지를 추가합니다.
    
    Task: #6

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -1,18 +1,26 @@
 import { Box, Container, Grid, Typography } from '@mui/material';
+import folder from '../../images/folder.png';
 
 export default function Banner() {
   return (
-    <Box sx={{ backgroundColor: "primary.main", color: "primary.contrastText" }}>
+    <Box sx={{ backgroundColor: 'primary.main', color: 'primary.contrastText' }}>
       <Container maxWidth="lg">
-        <Grid container direction="column">
-          <Grid item mt={8}>
-            <Typography variant="h3">Configure Hadoop/Spark Clusters</Typography>
+        <Grid container direction="row">
+          <Grid item xs={2.5} lg={2} sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}>
+            <img src={folder} width="150px" alt="banner" />
           </Grid>
-          <Grid item>
-            <Typography variant="h3">in a Cloud-Native Environment</Typography>
-          </Grid>
-          <Grid item mt={2} mb={8}>
-            <Typography variant="h5">Experience the highest speed in big data processing!</Typography>
+          <Grid item xs>
+            <Grid container direction="column">
+              <Grid item mt={8}>
+                <Typography variant="h3">Configure Hadoop/Spark Clusters</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="h3">in a Cloud-Native Environment</Typography>
+              </Grid>
+              <Grid item mt={2} mb={8}>
+                <Typography variant="h5">Experience the highest speed in big data processing!</Typography>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
