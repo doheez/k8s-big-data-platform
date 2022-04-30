a7c3ef43edac4f61104e2f9e29fda879256cc2b1
doheez <doheedev@gmail.com>
2022-05-01 03:27:44 +0900

    feat: Add divider to login with SNS text
    'Or Log in with SNS' 문구에 구분선을 추가합니다.
    
    Task: #16

--- client/src/components/Login/LoginSNS.js
+++ client/src/components/Login/LoginSNS.js
@@ -1,16 +1,28 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography, styled, Divider } from "@mui/material";
 import { ReactComponent as NaverLogo } from '../../images/naverLogo.svg';
 import { ReactComponent as KakaoLogo } from '../../images/kakaoLogo.svg';
 import { ReactComponent as GoogleLogo } from '../../images/googleLogo.svg';
 import { ReactComponent as FacebookLogo } from '../../images/facebookLogo.svg';
 
+const Root = styled('div')(({ theme }) => ({
+  width: '100%',
+  ...theme.typography.body2,
+  '& > :not(style) + :not(style)': {
+    marginTop: theme.spacing(2),
+  },
+}));
+
 export default function LoginSNS() {
 
   return (
     <Box mt={8}>
-      <Typography variant="h6" align="center">
-        Or Log in with SNS
-      </Typography>
+      <Root>
+        <Divider>
+          <Typography variant="subtitle1" align="center">
+            Or Log in with SNS
+          </Typography>
+        </Divider>
+      </Root>
       <Stack spacing={1.5} mt={3}>
         <Button variant="contained" size="large" color="naver">
           <NaverLogo width="30px" />
