176d500cd835883bc6f6cb37b2c9ef7b5e2347c6
doheez <doheedev@gmail.com>
2022-05-12 22:17:36 +0900

    feat: Create Data Grid for pods status table
    Pods Status 테이블을 생성하기 위해 My Page의
    Cluster Info 컴포넌트에 Data Grid를 삽입합니다.
    API 요청 전까지는 하드코딩한 임시 데이터를 활용합니다.
    
    Task: #25

--- client/src/components/MyPage/ClusterInfo.js
+++ client/src/components/MyPage/ClusterInfo.js
@@ -1,4 +1,5 @@
 import { Box, Button, Divider, List, ListItem, Stack, Typography, Grid } from "@mui/material";
+import PodTable from "./PodTable";
 
 export default function HadoopClusterInfo({ cluster }) {
   return (
@@ -14,20 +15,22 @@ export default function HadoopClusterInfo({ cluster }) {
         <ListItem>
           <Stack>
             <Typography variant="subtitle2" color="text.secondary">Numbers of Clusters</Typography>
-            <Typography color="text.primary">3</Typography>
+            <Typography color="text.primary">5</Typography>
           </Stack>
         </ListItem>
         <Divider />
         <ListItem>
-          <Stack>
+          <Stack width="100%">
             <Typography variant="subtitle2" color="text.secondary">Pods Status</Typography>
-            <Typography color="text.primary">테이블 삽입 예정</Typography>
+            <PodTable />
           </Stack>
         </ListItem>
         <Divider />
         <ListItem>
           <Stack>
-            <Typography variant="subtitle2" color="text.secondary">Throughput</Typography>
+            <Typography variant="subtitle2" color="text.secondary">
+              {cluster === 'hadoop' ? 'Size of Data' : 'Throughput'}
+            </Typography>
             <Typography color="text.primary">그래프 삽입 예정</Typography>
           </Stack>
         </ListItem>
@@ -36,8 +39,8 @@ export default function HadoopClusterInfo({ cluster }) {
           <Stack width="100%" rowGap={1}>
             <Typography variant="subtitle2" color="text.secondary">Cluster Management</Typography>
             <Grid container spacing={2}>
-              <Grid item xs={6}><Button variant="contained" fullWidth>add</Button></Grid>
-              <Grid item xs={6}><Button variant="contained" fullWidth>delete</Button></Grid>
+              <Grid item xs={6}><Button variant="contained" fullWidth>add cluster</Button></Grid>
+              <Grid item xs={6}><Button variant="contained" fullWidth>delete cluster</Button></Grid>
             </Grid>
           </Stack>
         </ListItem>
---
+++ client/src/components/MyPage/PodTable.js
@@ -0,0 +1,41 @@
+import { DataGrid } from '@mui/x-data-grid';
+import { Box } from '@mui/material';
+
+export default function PodTable() {
+  // 임시 데이터
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'name', headerName: 'Name', width: 180, },
+    { field: 'ready', headerName: 'Ready', width: 90, },
+    { field: 'status', headerName: 'Status', width: 130, },
+    { field: 'age', headerName: 'Age', width: 110, },
+  ];
+
+  const rows = [
+    { id: 1, name: 'find-animal-sdfk4r', ready: '1/1', status: 'RUNNING', age: '78ms' },
+    { id: 2, name: 'find-animal-sdfk4f', ready: '1/1', status: 'RUNNING', age: '78ms' },
+    { id: 3, name: 'find-animal-sdfk4t', ready: '1/1', status: 'RUNNING', age: '78ms' },
+    { id: 4, name: 'find-animal-sdfk4t', ready: '1/1', status: 'RUNNING', age: '78ms' },
+    { id: 5, name: 'find-animal-sdfk4t', ready: '1/1', status: 'RUNNING', age: '78ms' },
+  ];
+
+  return (
+    <Box width="100%" height={350} my={1}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+        sx={{
+          border: 2,
+          borderColor: 'primary.light',
+          '& .MuiDataGrid-cell:hover': {
+            color: 'primary.main',
+          },
+        }}
+      />
+    </Box>
+  );
+}
