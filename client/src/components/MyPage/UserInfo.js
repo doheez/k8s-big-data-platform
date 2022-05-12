8e35c7c599668197a913f79bff5664ef7f96688f
doheez <doheedev@gmail.com>
2022-05-12 11:59:04 +0900

    feat: Make up the User Info UI
    사용자 아바타, 사용자 이름, 사용자 이메일로 User Info UI를 구성합니다.
    하드코딩된 텍스트들은 추후 API 요청을 통해 실제 사용자 데이터로 대체할 예정입니다.
    
    Task: #25

--- client/src/components/MyPage/UserInfo.js
+++ client/src/components/MyPage/UserInfo.js
@@ -1,9 +1,17 @@
-import { Box } from "@mui/material";
+import { Avatar, Box, Stack, Typography } from "@mui/material";
 
 export default function UserInfo() {
   return (
-    <Box height={70} sx={{ backgroundColor: "mypageBox.main" }}>
-
+    <Box p={2} sx={{ backgroundColor: "mypageBox.main" }}>
+      <Stack direction="row" spacing={2}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Avatar>K</Avatar>
+        </Box>
+        <Stack>
+          <Typography variant="h6" color="text.primary">Apache Kim</Typography>
+          <Typography color="text.secondary">kubernetes@gmail.com</Typography>
+        </Stack>
+      </Stack>
     </Box>
   );
 }
