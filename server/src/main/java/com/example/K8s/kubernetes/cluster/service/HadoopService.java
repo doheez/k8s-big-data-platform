115dc1445bb108b8d695867f9b39cb77b3f1abcb
idoburnish <doyun7433@naver.com>
2022-05-18 16:39:34 +0900

    fix: modify DB connection error

--- server/src/main/java/com/example/K8s/kubernetes/cluster/service/HadoopService.java
+++ server/src/main/java/com/example/K8s/kubernetes/cluster/service/HadoopService.java
@@ -2,10 +2,14 @@ package com.example.K8s.kubernetes.cluster.service;
 
 import com.example.K8s.kubernetes.cluster.controller.ClusterController;
 import com.example.K8s.kubernetes.cluster.dto.ClusterRegDto;
+import com.example.K8s.kubernetes.cluster.hadoopcr.HadoopCr;
 import com.example.K8s.kubernetes.cluster.model.Cluster;
 import com.example.K8s.kubernetes.cluster.model.Hadoop;
 import com.example.K8s.kubernetes.cluster.repository.ClusterRepository;
 import com.example.K8s.kubernetes.cluster.repository.HadoopRepository;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.apis.CustomObjectsApi;
+import io.kubernetes.client.util.ClientBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,10 +21,9 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class HadoopService {
 
-    private final ClusterController clusterController;
     private final ClusterRepository clusterRepository;
     private final HadoopRepository hadoopRepository;
-    private final HadoopClusterService hadoopClusterService;
+//    private final HadoopClusterService hadoopClusterService;
 
     // hadoop 클러스터 생성
     public boolean createHadoopCluster(ClusterRegDto regDto) throws IOException {
@@ -30,7 +33,7 @@ public class HadoopService {
 
         // 클러스터 생성
         Cluster newCluster = new Cluster(regDto);
-        boolean success = hadoopClusterService.createHadoopCluster(newCluster);
+        boolean success = createHadoopCluster(newCluster);
         if (!success) return false;
         clusterRepository.save(newCluster);
 
@@ -38,5 +41,30 @@ public class HadoopService {
         Hadoop hadoop = new Hadoop(newCluster);
         hadoopRepository.save(hadoop);
         return true;
+    }
+
+    public boolean createHadoopCluster(Cluster cluster) throws IOException {
+        String name = cluster.getName();
+        int amount = cluster.getAmount();
+
+        CustomObjectsApi apiInstance = new CustomObjectsApi(ClientBuilder.standard().build());
+        String group = "alicek106.hadoop";
+        String version = "v1alpha1";
+        String namespace = "hadoop";
+        String plural = "hadoopservices";
+        Object body = new HadoopCr(name, amount);
+
+        try {
+            Object result = apiInstance.createNamespacedCustomObject(group, version, namespace, plural, body, null, null, null);
+            System.out.println(result);
+            return true;
+        } catch (ApiException e) {
+            System.err.println("Exception when calling CustomObjectsApi#patchNamespacedCustomObject");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+            return false;
+        }
     }
 }
