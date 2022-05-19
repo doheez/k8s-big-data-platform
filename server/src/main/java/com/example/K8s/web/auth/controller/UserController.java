64c40200bea2036ae87d86a8c81652e6bc004766
HyeW <qorgp13@naver.com>
2022-05-19 11:20:11 +0900

    Revert "feat: Add login api controller"
    This reverts commit 300f61b7628c4c7d63b06e9eb071de25f9b46a89.

--- server/src/main/java/com/example/K8s/web/auth/controller/UserController.java
+++ server/src/main/java/com/example/K8s/web/auth/controller/UserController.java
@@ -2,8 +2,7 @@ package com.example.K8s.web.auth.controller;
 
 import com.example.K8s.web.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +11,19 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 @RestController
 public class UserController {
-    private final HttpSession httpSession;
+    //private final HttpSession httpSession;
 
-    @GetMapping("/api/v1/user")
-    public ResponseEntity<?> getUser() {
-        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
-        if (sessionUser != null) {
-            System.out.println(sessionUser.getEmail());
-        } else {
-            System.out.println("null입니다.");
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(sessionUser);
-    }
-
+    // Model 객체
+    // 서버 탬플릿 엔진에서 사용가능한 객체를 저장할 수 있다.
+//    @GetMapping("/")
+//    public String index(Model model) {
+//
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        // 세션에 저장된 값이 있을 경우만 model에 userName으로 등록한다.
+//        if (user != null) {
+//            model.addAttribute("userName",user.getName());
+//            model.addAttribute("userEmail",user.getEmail());
+//        }
+//        return "test";
+//    }
 }
