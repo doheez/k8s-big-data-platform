b363edb245f52c490e9f620e44d097a791c9911d
heohyeonjin <hyeonjin4870@naver.com>
2022-05-28 23:59:38 +0900

    fix: Modify cluster create&adjust API
    back-end와 k8s-engine간의 spark 클러스터 생성 API 수정
    back-end와 k8s-engine간의 spark 클러스터 수정 API 수정

--- server/src/main/java/com/example/K8s/kubernetes/cluster/service/SparkAdjustService.java
+++ server/src/main/java/com/example/K8s/kubernetes/cluster/service/SparkAdjustService.java
@@ -9,11 +9,13 @@ import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.apis.CustomObjectsApi;
 import io.kubernetes.client.util.ClientBuilder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SparkAdjustService {
@@ -27,14 +29,14 @@ public class SparkAdjustService {
 
         // 클러스터 개수 수정
         Cluster cluster = clusterRepository.findClusterByName(adjDto.getName());
-        boolean success = adjust_API(cluster);
+        boolean success = adjust_API(cluster, adjDto.getAmount());
         if(!success) return false;
 
-        // 클러스터 수정 개수 저장
+        // 클러스터 개수 수정 정보 저장
         cluster.setAmount(adjDto.getAmount());
         return true;
     }
-    public boolean adjust_API (Cluster cluster) throws IOException{
+    public boolean adjust_API (Cluster cluster, int replace_amount) throws IOException{
         CustomObjectsApi apiInstance = new CustomObjectsApi(ClientBuilder.standard().build());
         String group ="radanalytics.io";
         String version ="v1";
@@ -44,11 +46,10 @@ public class SparkAdjustService {
         try {
             Object getobject = apiInstance.getNamespacedCustomObject(group, version, namespace, plural, name);
             String resourceVersion = Parsing(getobject);
-            Object body = resetCluster(cluster.getName(), cluster.getAmount(),resourceVersion);
-            Object result = apiInstance.replaceNamespacedCustomObjectScale(group, version, namespace, plural, name, body, null, null);
+            Object body = resetCluster(cluster.getName(), replace_amount,resourceVersion);
+            Object result = apiInstance.replaceNamespacedCustomObject(group, version, namespace, plural, name, body, null, null);
             System.out.println(result);
         } catch (ApiException e) {
-            System.err.println("Exception when calling CustomObjectsApi#replaceNamespacedCustomObjectScale");
             System.err.println("Status code: " + e.getCode());
             System.err.println("Reason: " + e.getResponseBody());
             System.err.println("Response headers: " + e.getResponseHeaders());
