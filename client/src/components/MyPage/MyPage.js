596664934229d4b477dec780a9426a4bc1a7a3b0
doheez <doheedev@gmail.com>
2022-05-29 17:40:07 +0900

    feat: Add loading component to MyPage
    마이페이지에서 클러스터 기본 정보를 서버로부터 모두 받아올 때까지
    CircularProgress와 함께 로딩 문구를 띄웁니다.
    
    Task: #101

--- client/src/components/MyPage/MyPage.js
+++ client/src/components/MyPage/MyPage.js
@@ -1,6 +1,6 @@
 import UserInfo from './UserInfo';
 import ClusterInfo from './ClusterInfo';
-import { Container, Grid, Typography } from '@mui/material';
+import { Container, Grid, Typography, Stack, CircularProgress } from '@mui/material';
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
@@ -36,6 +36,7 @@ export default function MyPage() {
           <Typography variant="h6" color="primary.main">User Info</Typography>
           <UserInfo />
         </Grid>
+        {hadoopClusterList.length > 0 ?
           <Grid item xs md container direction="column" spacing={{ xs: 0, md: 3 }}>
             <Grid item xs>
               <Typography variant="h6" color="primary.main">Hadoop Cluster Info</Typography>
@@ -46,6 +47,17 @@ export default function MyPage() {
               <ClusterInfo cluster={SPARK} clusterList={sparkClusterList} />
             </Grid>
           </Grid>
+          :
+          <Grid item xs md container direction="column" spacing={{ xs: 0, md: 3 }}>
+            <Stack alignItems="center" mt={8}>
+              <CircularProgress />
+              <Typography m={2}>
+                {'Loading Cluster Info...'}
+              </Typography>
+            </Stack>
+          </Grid>
+        }
+
       </Grid>
     </Container>
   );
