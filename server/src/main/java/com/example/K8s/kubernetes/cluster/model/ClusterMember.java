77ef94bf4f6dd5242a62e0c1521186040342bdfd
heohyeonjin <hyeonjin4870@naver.com>
2022-05-30 19:36:45 +0900

    feat: add ClusterMember database
    클러스터 공동 관리를 위한 데이터베이스 설계 추가

---
+++ server/src/main/java/com/example/K8s/kubernetes/cluster/model/ClusterMember.java
@@ -0,0 +1,52 @@
+package com.example.K8s.kubernetes.cluster.model;
+
+import com.example.K8s.web.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class ClusterMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="cluster_user_id")
+    private Long clusterMemberId;
+
+    // Cluster
+    @ManyToOne(targetEntity = Cluster.class, fetch=FetchType.LAZY)
+    @JoinColumn(name="cluster_id")
+    private Cluster cluster;
+
+    //User
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+
+    // 연관관계 편의 메서드
+    public void setCluster(Cluster cluster){
+        if(this.cluster!=null){
+            this.cluster.getClusterMembers().remove(this);
+        }
+        this.cluster = cluster;
+        cluster.getClusterMembers().add(this);
+    }
+
+    public void setUser(User user){
+        if(this.user!=null){
+            this.user.getClusterMembers().remove(this);
+        }
+        this.user=user;
+        user.getClusterMembers().add(this);
+    }
+
+    public ClusterMember(Cluster cluster, User user){
+        setCluster(cluster);
+        setUser(user);
+    }
+
+}
