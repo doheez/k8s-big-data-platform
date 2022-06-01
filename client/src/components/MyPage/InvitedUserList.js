4608fe2ae521f3f8bc266e24a81e2fa684cac236
doheez <doheedev@gmail.com>
2022-06-01 21:14:01 +0900

    refactor: Seperate util functions from component
    유저 정보 컴포넌트와 초대된 유저 리스트 컴포넌트에서
    공통적으로 사용하는 유틸 함수를 새 파일로 분리하여 재사용합니다.
    
    Task: #121

--- client/src/components/MyPage/InvitedUserList.js
+++ client/src/components/MyPage/InvitedUserList.js
@@ -1,37 +1,7 @@
 import { Avatar, AvatarGroup, Tooltip } from "@mui/material";
+import { stringAvatar } from "./Utils/Utils";
 
-export default function InvitedUserList({ userList }) {
-    function stringToColor(string) {
-        let hash = 0;
-        let i;
-
-        /* eslint-disable no-bitwise */
-        for (i = 0; i < string.length; i += 1) {
-            hash = string.charCodeAt(i) + ((hash << 5) - hash);
-        }
-
-        let color = '#';
-
-        for (i = 0; i < 3; i += 1) {
-            const value = (hash >> (i * 8)) & 0xff;
-            color += `00${value.toString(16)}`.slice(-2);
-        }
-        /* eslint-enable no-bitwise */
-
-        return color;
-    }
-
-    function stringAvatar(name) {
-        if (name === undefined) {
-            return;
-        }
-        return {
-            sx: {
-                bgcolor: stringToColor(name),
-            },
-            children: name[0].toUpperCase()
-        };
-    }
+const InvitedUserList = ({ userList }) => {
 
     return (
         <AvatarGroup max={10}>
@@ -43,3 +13,5 @@ export default function InvitedUserList({ userList }) {
         </AvatarGroup>
     );
 }
+
+export default InvitedUserList;
--- client/src/components/MyPage/UserInfo.js
+++ client/src/components/MyPage/UserInfo.js
@@ -1,6 +1,7 @@
 import { Avatar, Box, Stack, Typography } from "@mui/material";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { stringAvatar } from "./Utils/Utils";
 
 export default function UserInfo() {
   const [name, setName] = useState();
@@ -27,38 +28,6 @@ export default function UserInfo() {
   useEffect(() => {
     getUserInfo();
   }, [name]);
-
-  function stringToColor(string) {
-    let hash = 0;
-    let i;
-
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = '#';
-
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.slice(-2);
-    }
-    /* eslint-enable no-bitwise */
-
-    return color;
-  }
-
-  function stringAvatar(name) {
-    if (name === undefined) {
-      return;
-    }
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-      },
-      children: name[0].toUpperCase()
-    };
-  }
 
   return (
     <Box p={2} sx={{ backgroundColor: "mypageBox.main", borderRadius: 1 }}>
